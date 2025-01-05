e64a198899b4cf5c60d5471909390ff557051c27
Yuragranovsky <yuragranovsky@gmail.com>
2025-01-05 13:13:20 +0200

    code before lunch

--- src/main/java/telran/employees/db/jpa/CompanyRepositoryJpaImpl.java
+++ src/main/java/telran/employees/db/jpa/CompanyRepositoryJpaImpl.java
@@ -1,22 +1,43 @@
 package telran.employees.db.jpa;
 
+import java.lang.reflect.Constructor;
 import java.util.*;
 
 import jakarta.persistence.*;
+import jakarta.persistence.spi.PersistenceProvider;
 import jakarta.persistence.spi.PersistenceUnitInfo;
 import telran.employees.Employee;
 import telran.employees.db.CompanyRepository;
 
-public class CompanyRepositoryJpaImpl implements CompanyRepository{
+public class CompanyRepositoryJpaImpl implements CompanyRepository {
     private EntityManager em;
+
     public CompanyRepositoryJpaImpl(PersistenceUnitInfo persistenceUnit,
-     HashMap<String, Object> properties) {
-        
+            HashMap<String, Object> properties) {
+        try {
+            String providerName = persistenceUnit.getPersistenceUnitName();
+            @SuppressWarnings("unchecked")
+            Class<PersistenceProvider> clazz = (Class<PersistenceProvider>) Class.forName(providerName);
+            Constructor<PersistenceProvider> constructor = clazz.getConstructor();
+            PersistenceProvider provider = constructor.newInstance();
+            EntityManagerFactory emf = provider.createContainerEntityManagerFactory(persistenceUnit, properties);
+            em = emf.createEntityManager();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
     @Override
     public List<Employee> getEmployees() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getEmployees'");
+        TypedQuery<EmployeeEntity> query = em.createQuery("select empl from EmployeeEntity empl",
+                EmployeeEntity.class);
+        return toEmployeeList(query.getResultList());
+    }
+
+    private List<Employee> toEmployeeList(List<EmployeeEntity> resultList) {
+        return resultList.stream().map(EmployeesMapper::toEmployeeDtoFromEntity).toList();
     }
 
 }
